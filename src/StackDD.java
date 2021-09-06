a221ac7a90b2f40960d8f7810ab0cd94ef4f1f35
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2021-09-06 07:50:48 -0400

    Added attributes, constructor, and isEmpty method for StackDD class

--- src/StackDD.java
+++ src/StackDD.java
@@ -1,4 +1,24 @@
 
-public class StackDD {
+import java.util.ArrayList;
 
+public class StackDD {
+	
+	private final int MAX_NUMBER_OF_OBJECTS = 5;
+	private int top;
+	
+	// Creating a stack with its maximum size
+	private Integer stack[] = new Integer[MAX_NUMBER_OF_OBJECTS];
+	
+	// Constructor
+	StackDD() {
+		top = -1;
+	}
+	
+	// isEmpty method
+	public boolean isEmpty() {
+		return (top < 0);
+	}
+	
+	
+	
 }
