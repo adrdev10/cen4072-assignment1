d7de427a8d60b6e6f3a12a1055f4f620b9c8b7d4
SirTetsuno <42874751+SirTetsuno@users.noreply.github.com>
2021-09-08 15:55:33 -0400

    Create clientTwo.java

---
+++ src/clientTwo.java
@@ -0,0 +1,25 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*----------------------------------
+| Goal: Document and have the
+| program run even when client
+| enters a response other than
+| an integer
+----------------------------------*/
+
+
+public class clientTwo
+{
+    public static void main(String[] args)
+    {
+        clientOne stackTest = new clientOne();
+
+        System.out.println("\nHello, this code will demonstrate how Defensive Design (StackDD) ");
+        System.out.println("works! Since this is a stack, you have the option to either push, ");
+        System.out.println("pop, peek, or print the stack depending on your choice!");
+
+
+        stackTest.startProgram();
+        }
+}
