5430a893e8e8016244217ef450ea5c941a86607d
ryanpereda <ryanpereda9001@gmail.com>
2021-09-13 17:13:15 -0400

    added methods to StackDCTest

--- src/StackDCTest.java
+++ src/StackDCTest.java
@@ -4,4 +4,65 @@ import org.junit.jupiter.api.Test;
 
 public class StackDCTest {
 	
+	//Testing isEmpty method
+	
+	@Test
+	void isEmptyTest1() {
+		
+	}
+	
+	void isEmptyTest2() {
+		
+	}
+	
+	//Testing isFull method
+	
+	@Test
+	void isFullTest1() {
+		
+	}
+	
+	void isFullTest2() {
+		
+	}
+	
+	//Testing emptyStack method
+	
+	@Test
+	void emptyStackTest1() {
+		
+	}
+		
+	//Testing push method
+	
+	@Test
+	void pushTest1() {
+		
+	}
+	
+	//Testing pop method
+	
+	@Test
+	void popTest1() {
+		
+	}
+	
+	void popTest2() {
+		
+	}
+	
+	//Testing peek method
+	
+	@Test
+	void peekTest1() {
+		
+	}
+	
+	//Testing printStack method
+	
+	@Test
+	void printStackTest1() {
+		
+	}
+	
 }
